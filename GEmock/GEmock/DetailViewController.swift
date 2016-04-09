f20af587fd693767299a56b419aeee9c9453485b
Elenabunny <elenabunny73@gmail.com>
2016-04-09 16:18:52 -0400

    Add email attachment function

--- GEmock/GEmock/DetailViewController.swift
+++ GEmock/GEmock/DetailViewController.swift
@@ -11,11 +11,66 @@ import MessageUI
 import Foundation
 
 class DetailViewController: UIViewController, MFMailComposeViewControllerDelegate  {
-    
-    
+    //
+    private enum MIMEType: String {
+        case csv = "text/csv"
+        case jpg = "image/jpeg"
+        case png = "image/png"
+        case doc = "application/msword"
+        case ppt = "application/vnd.ms-powerpoint"
+        case html = "text/html"
+        case pdf = "application/pdf"
+        
+        init?(type: String) {
+            switch type.lowercaseString {
+            case "csv": self = .csv
+            case "jpg": self = .jpg
+            case "png": self = .png
+            case "doc": self = .doc
+            case "ppt": self = .ppt
+            case "html": self = .html
+            case "pdf": self = .pdf
+            default: return nil
+            }
+        }
+    }
     
     @IBAction func export(sender: AnyObject) {
-        let mailComposeViewController = configuredMailComposeViewController()
+        
+        //Check to see the device can send email.
+        if( MFMailComposeViewController.canSendMail() ) {
+            print("Can send email.")
+            
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.mailComposeDelegate = self
+            
+            //Set the subject and message of the email
+            mailComposer.setSubject("Have you heard a swift?")
+            mailComposer.setMessageBody("This is what they sound like.", isHTML: false)
+            
+            if let filePath = NSBundle.mainBundle().pathForResource("attendence", ofType: "csv") {
+                print("File path loaded.")
+                
+                if let fileData = NSData(contentsOfFile: filePath) {
+                    print("File data loaded.")
+                    mailComposer.addAttachmentData(fileData, mimeType: "csv", fileName: "attendence")
+                }
+            }
+            self.presentViewController(mailComposer, animated: true, completion: nil)
+        }
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+        /*
+        //
+        let mailComposeViewController = configuredMailComposeViewController(attachmentName: String)
+        //
+        let csvName = "attendance.csv"
+        //
+        configuredMailComposeViewController(csvName)
         if MFMailComposeViewController.canSendMail() {
             self.presentViewController(mailComposeViewController, animated: true, completion: nil)
         } else {
@@ -23,7 +78,7 @@ class DetailViewController: UIViewController, MFMailComposeViewControllerDelegat
         }
     }
     
-    func configuredMailComposeViewController() -> MFMailComposeViewController {
+    func configuredMailComposeViewController(attachmentName: String) -> MFMailComposeViewController {
         let mailComposerVC = MFMailComposeViewController()
         mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
         
@@ -31,6 +86,19 @@ class DetailViewController: UIViewController, MFMailComposeViewControllerDelegat
         mailComposerVC.setSubject("Sending you an in-app e-mail...")
         mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
         
+        //
+        let fileParts = attachmentName.componentsSeparatedByString(".")
+        let fileName = fileParts[0]
+        let fileExtension = fileParts[1]
+        
+        if let filePath = NSBundle.mainBundle().pathForResource(fileName, ofType: fileExtension) {
+            print("file path loaded")
+            if let fileData = NSData(contentsOfFile: filePath), mimeType = MIMEType(type: fileExtension) {
+                print("file data loaded")
+                mailComposerVC.addAttachmentData(fileData, mimeType: mimeType.rawValue, fileName: fileName)
+                
+                self.presentViewController(mailComposerVC, animated: true, completion: nil)
+            }
         return mailComposerVC
     }
     
@@ -44,13 +112,15 @@ class DetailViewController: UIViewController, MFMailComposeViewControllerDelegat
         controller.dismissViewControllerAnimated(true, completion: nil)
     }
     
-    override func viewDidLoad() {
+        */
+        
+   override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+   override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
