7221566ff0b3c2cf78d8559d87765fb45e637b82
HaoHaoGitHub <chhao.rpi@gmail.com>
2016-04-25 19:34:21 -0400

    Changed Color and leave out the choose existing function

--- GEmock/GEmock/ScanViewController.swift
+++ GEmock/GEmock/ScanViewController.swift
@@ -81,7 +81,7 @@ class ScanViewController: UIViewController, UITextViewDelegate, UINavigationCont
         view.endEditing(true)
         //moveViewDown()
         // 2
-        let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Photo",
+        let imagePickerActionSheet = UIAlertController(title: "Snap Photo",
                                                        message: nil, preferredStyle: .ActionSheet)
         // 3
         if UIImagePickerController.isSourceTypeAvailable(.Camera) {
@@ -109,14 +109,14 @@ class ScanViewController: UIViewController, UITextViewDelegate, UINavigationCont
             imagePickerActionSheet.addAction(cameraButton)
         }
         // 4
-        let libraryButton = UIAlertAction(title: "Choose Existing", style: .Default) {
-            (alert) -> Void in
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = .PhotoLibrary
-            self.presentViewController(imagePicker, animated: true, completion: nil)
-        }
-        imagePickerActionSheet.addAction(libraryButton)
+//        let libraryButton = UIAlertAction(title: "Choose Existing", style: .Default) {
+//            (alert) -> Void in
+//            let imagePicker = UIImagePickerController()
+//            imagePicker.delegate = self
+//            imagePicker.sourceType = .PhotoLibrary
+//            self.presentViewController(imagePicker, animated: true, completion: nil)
+//        }
+//        imagePickerActionSheet.addAction(libraryButton)
         // 5
         let cancelButton = UIAlertAction(title: "Cancel", style: .Cancel) { (alert) -> Void in
         }
